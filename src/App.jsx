a9501ab5dc665abdce25109923d9ac6db1f6a97d
tiedsandi <fachransandi@gmail.com>
2024-11-10 09:41:45 +0700

    final project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import {useState} from 'react';
+import items from './data';
+import Title from './Title';
+import Categories from './Categories';
+import Menu from './Menu';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
-  return <main></main>;
+  const [menuItems, setMenuItems] = useState(items);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className='menu'>
+        <Title text='our menu' />
+        <Categories categories={allCategories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
